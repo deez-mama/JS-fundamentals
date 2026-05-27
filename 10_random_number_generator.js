fb0d46215a8ecc93f2ec3e954e3be44716173d1a
deez-mama <bishowjeetb@gmail.com>
2026-05-27 15:23:50 +0545

    halka mistake

--- 10_random_number_generator.js
+++ 10_random_number_generator.js
@@ -3,6 +3,6 @@
 // and then round it down to the nearest whole number using Math.floor() and add 1 to get a number between 1 and 6.
 let randomNumber = Math.floor(Math.random() * 6) + 1; //Added 1 because generally the output is 0 to 5 but we want 1 to 6
 
-conosle.log(randomNumber);
+console.log(randomNumber);
 
 //Change the "6" to any number depending upon the range needed
