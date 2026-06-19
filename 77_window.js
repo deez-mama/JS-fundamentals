c2c7b5ebfc5a10be159c7e59da2a54d3d145e937
deez-mama <bishowjeetb@gmail.com>
2026-06-19 21:56:50 +0545

    window

---
+++ 77_window.js
@@ -0,0 +1,21 @@
+// // window -> interface used to talk to the web browswer
+// //            DOM is a property of the window
+
+console.dir(window);
+// console.log(window.innerWidth);
+// console.log(window.innerHeight);
+// console.log(window.scrollX); //how much we have scrolled in X direction
+// console.log(window.scrollY); // """""" in Y direction
+
+// console.log(window.location.href);
+// console.log(window.location.href); //web address
+// window.location.href = "https://google.come" //redirects to google.com
+
+// console.log(window.location.hostname); //gives hostname i.e local IP address 
+// console.log(window.location.pathname); // results /index.html
+
+
+// // The following codes can be used with a button such that they can be invoked when a certain button is pressed
+// window.open(); //opens new tab
+// window.close(); //closes the current tab
+// window.print(); // opens printing dialog box
